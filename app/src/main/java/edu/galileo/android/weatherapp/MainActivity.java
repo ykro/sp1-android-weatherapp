b1096e3a2febe540cb43b157233172c9f2364287
ykro <aacs85@gmail.com>
2016-01-19 19:58:02 -0600

    adding button and edittext implementation WIP

--- app/src/main/java/edu/galileo/android/weatherapp/MainActivity.java
+++ app/src/main/java/edu/galileo/android/weatherapp/MainActivity.java
@@ -1,8 +1,11 @@
 package edu.galileo.android.weatherapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     private final static String TAG = "MainActivity";
@@ -11,6 +14,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.e(TAG, "onCreate");
+        final EditText inputCity = (EditText)
+                            findViewById(R.id.inputCity);
+        Button btnSubmit = (Button)
+                            findViewById(R.id.btnSubmit);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String strInput = inputCity.getText().toString();
+                Log.e(TAG, strInput);
+            }
+        });
+
     }
 }

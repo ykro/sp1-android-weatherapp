c84c6d8100c3d12587a406ba33432825d5857df0
ykro <aacs85@gmail.com>
2016-01-21 18:27:01 -0600

    refactoring to use butterknife for button click

--- app/src/main/java/edu/galileo/android/weatherapp/MainActivity.java
+++ app/src/main/java/edu/galileo/android/weatherapp/MainActivity.java
@@ -3,32 +3,26 @@ package edu.galileo.android.weatherapp;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
     private final static String TAG = "MainActivity";
     @Bind(R.id.inputCity) EditText inputCity;
-    @Bind(R.id.btnSubmit) Button btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+    }
 
-        btnSubmit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String strInput = inputCity.getText().toString();
-
-                Log.e(TAG, strInput);
-            }
-        });
-
+    @OnClick(R.id.btnSubmit)
+    public void handleClick(){
+        String strInput = inputCity.getText().toString();
+        Log.e(TAG, strInput);
     }
 }

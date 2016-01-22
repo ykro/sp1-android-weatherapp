736d2d302f963f1a0caf933f0ba4a3200edd091c
ykro <aacs85@gmail.com>
2016-01-21 19:19:16 -0600

    adding weatherinfo pojo

---
+++ app/src/main/java/edu/galileo/android/weatherapp/WeatherInfo.java
@@ -0,0 +1,70 @@
+package edu.galileo.android.weatherapp;
+
+/**
+ * Created by ykro.
+ */
+public class WeatherInfo {
+    private String description;
+    private String iconName;
+    private String temp;
+    private String tempMin;
+    private String tempMax;
+    private String sunrise;
+    private String sunset;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getIconName() {
+        return iconName;
+    }
+
+    public void setIconName(String iconName) {
+        this.iconName = iconName;
+    }
+
+    public String getTemp() {
+        return temp;
+    }
+
+    public void setTemp(String temp) {
+        this.temp = temp;
+    }
+
+    public String getTempMin() {
+        return tempMin;
+    }
+
+    public void setTempMin(String tempMin) {
+        this.tempMin = tempMin;
+    }
+
+    public String getTempMax() {
+        return tempMax;
+    }
+
+    public void setTempMax(String tempMax) {
+        this.tempMax = tempMax;
+    }
+
+    public String getSunrise() {
+        return sunrise;
+    }
+
+    public void setSunrise(String sunrise) {
+        this.sunrise = sunrise;
+    }
+
+    public String getSunset() {
+        return sunset;
+    }
+
+    public void setSunset(String sunset) {
+        this.sunset = sunset;
+    }
+}
